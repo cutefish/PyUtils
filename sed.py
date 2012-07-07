18d27859cbc3069467fcef62c466deb59e2f9dc9
cutefish <cutefish.yx@gmail.com>
2012-07-06 23:22:35 -0400

    deleted:    __init__.py 	modified:   sed.py 	new file:   sudo.py

--- sed.py
+++ sed.py
@@ -15,7 +15,8 @@ def replaceWith(inPath, outPath, pattern, replace, maxNumReplace=0):
     @param inPath, input file path
     @param outPath, output file path, can be the same with inPath
     @param pattern, regex string or compiled regex
-    @param replace, replace string
+    @param replace, replace string.
+      Use '\number' to represent group in @pattern.
     @param maxNumReplace, max number of replacement, default replace all.
 
     @return number of replacement.
@@ -28,60 +29,24 @@ def replaceWith(inPath, outPath, pattern, replace, maxNumReplace=0):
     else:
         tmpPath = outPath
     outputFile = Path(tmpPath).open('w')
-    regex = re.compile(pattern)
+    patternRe = re.compile(pattern)
     count = 0
     for line in inputFile:
-        numMatches = len(re.findall(regex, line))
+        numMatches = len(re.findall(patternRe, line))
         #if no match write to output
         if numMatches == 0:
             outputFile.write(line)
             continue
-        if count < maxNumReplace:
+        #if already replaced enough
+        if (count >= maxNumReplace) and (maxNumReplace != 0):
+            outputFile.write(line)
+            continue
+        if (maxNumReplace != 0):
             num = min(maxNumReplace - count, numMatches)
         else:
             num = numMatches
-        outputFile.write(re.sub(regex, replace, line, num))
+        outputFile.write(re.sub(patternRe, replace, line, num))
         count += num
-        if maxNumReplace == 0:
-            continue
-        if count >= maxNumReplace:
-            break
-    inputFile.close()
-    outputFile.close()
-    if (inPath == outPath):
-        Path.move(tmpPath, outPath)
-    return count
-
-def appendLine(inPath, outPath, pattern, append, maxNumAppend=0):
-    """Append after a line in file.
-    @param inPath, input file path
-    @param outPath, output file path, can be the same with inPath
-    @param pattern, regex string or compiled regex
-    @param append, append string, adding '\n' automatically
-    @param maxNumAppend, max number of append, default append all.
-
-    @return number of append.
-    """
-    Path.isFileOrDie(inPath)
-    inputPath = Path(inPath)
-    inputFile = inputPath.open('r')
-    if (inPath == outPath):
-        tmpPath = '/tmp/_%s_temporary' %inputPath.baseName()
-    else:
-        tmpPath = outPath
-    outputFile = Path(tmpPath).open('w')
-    regex = re.compile(pattern)
-    count = 0
-    lineToAppend = append + '\n'
-    for line in inputFile:
-        #if no match write to output
-        outputFile.write(line)
-        if re.search(pattern, line) == None:
-            continue
-        outputFile.write(lineToAppend)
-        count += 1
-        if count >= maxNumAppend:
-            break
     inputFile.close()
     outputFile.close()
     if (inPath == outPath):
@@ -89,8 +54,18 @@ def appendLine(inPath, outPath, pattern, append, maxNumAppend=0):
     return count
 
 def main():
-    print appendLine("~/t", "~/t", "[0-9]+", "Hello", 3)
-    #print replaceWith("~/t", "~/t", "[0-9]+", "abc", 6)
+    testFile = ("methods:\n"
+                "void function1()\n"
+                "void function2()\n"
+                "void function3()\n"
+                "void functionfunction4()\n"
+               )
+    path = Path("~/t")
+    f = path.open('w')
+    f.write(testFile)
+    f.close()
+    print replaceWith(path, path, r'(void function[0-9]+)()', r'//\1()\n\1(void)')
+    print replaceWith(path, path, r'function', r'func', 7)
 
 if __name__ == '__main__':
     main()
